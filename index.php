7f1f9ae7e3837f92d2f1fc5ec8992ab0e5605f43
Chad Miller <chad.miller.3333@gmail.com>
2017-04-07 15:06:19 -0400

    Mass renaming

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="refresh" content="0;url=pages/home.php">
+<meta http-equiv="refresh" content="0;url=planner/index.php">
 <title>Event Planning</title>
 <script language="javascript">
-    window.location.href = "pages/home.php"
+    window.location.href = "planner/index.php"
 </script>
 </head>
 <body>
-Go to <a href="pages/home.php">/pages/home.php</a>
+Go to <a href="planner/index.php">/planner/index.php</a>
 </body>
 </html>
