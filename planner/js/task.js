a910583d004d5603a4399bca7d5521cb181f38a7
Chad Miller <chad.miller.3333@gmail.com>
2017-04-09 23:41:08 -0400

    Task page complete

--- planner/js/task.js
+++ planner/js/task.js
@@ -23,51 +23,10 @@ function task_initialize()
     );
 }
 
-function populate_task_list_table( data, pagination )
+function add_new_row()
 {
     var task_list_tbody = $( '#task_list_tbody' );
-
-    task_list_tbody.empty();
-
-    $.each( data, function( i, event_needed_role ) {
-        var event_needed_role_pk = event_needed_role['event_needed_role'];
-        var row                  = $( '<tr id="' + event_needed_role_pk + '">' );
-
-        var task_name = $( '<td>' ).text( event_needed_role['needed_role_name'] );
-        var actions   = $( '<td>' );
-
-        var edit_name     = $( '<input type="button" value="Edit Assignments" class="edit">' );
-        var delete_button = $( '<input type="button" value="Delete" class="delete">' );
-
-        edit_name.click( function() {
-            $.featherlight(
-                "/planner/modal/edit_task.php?event=" + TEST_EVENT + "&role=" + event_needed_role['needed_role'],
-                {
-                    'beforeClose' : perform_edit_task_update // defined in edit_task.js
-                }
-            );
-        });
-
-        delete_button.click( function() {
-            $( '#' + event_needed_role_pk ).hide();
-
-            // TODO ajax delete
-        });
-
-        actions.append(
-            edit_name,
-            delete_button
-        );
-
-        row.append(
-            task_name,
-            actions
-        );
-
-        task_list_tbody.append( row );
-    });
-
-    var row = $( '<tr>' );
+    var row             = $( '<tr id="new">' );
 
     var role_name_input = $( '<td>' )
         .append( '<input type="text" id="new_name">' );
@@ -85,9 +44,122 @@ function populate_task_list_table( data, pagination )
     task_list_tbody.append( row );
 }
 
+function create_action_set( tr, event_needed_role, needed_role )
+{
+    var actions = $( '<td>' );
+
+    var edit_name     = $( '<input type="button" value="Edit Assignments" class="edit">' );
+    var delete_button = $( '<input type="button" value="Delete" class="delete">' );
+
+    edit_name.click( function() {
+        $.featherlight(
+            "/planner/modal/edit_task.php?event=" + TEST_EVENT + "&role=" + needed_role,
+            {
+                'beforeClose' : perform_edit_task_update // defined in edit_task.js
+            }
+        );
+    });
+
+    delete_button.click( function() {
+        purge_role( tr, needed_role );
+    });
+
+    actions.append(
+        edit_name,
+        delete_button
+    );
+
+    return actions;
+}
+
+function purge_role( tr, needed_role )
+{
+    tr.hide();
+
+    $.ajax( {
+        'type'     : 'DELETE',
+        'url'      : 'http://planmything.tech/api/event/' + TEST_EVENT + '/roles/' + needed_role,
+        'dataType' : 'json'
+    });
+
+    var get_url = 'http://planmything.tech/api/event/' + TEST_EVENT + '/guests/?role=' + needed_role;
+    var entities = [];
+
+    $.get( get_url, null, function( response, textStatus, jqXHR ) {
+        for( var record in response )
+            entities.push( record['entity'] );
+    }, 'json');
+
+    for( var entity in entities )
+    {
+        $.ajax( {
+            'type'     : 'DELETE',
+            'url'      : 'http://planmything.tech/api/event/' + TEST_EVENT + '/guests/' + entity + '/roles/' + needed_role,
+            'dataType' : 'json'
+        });
+    }
+}
+
+function populate_task_list_table( data, pagination )
+{
+    var task_list_tbody = $( '#task_list_tbody' );
+
+    task_list_tbody.empty();
+
+    $.each( data, function( i, event_needed_role ) {
+        var event_needed_role_pk = event_needed_role['event_needed_role'];
+        var row                  = $( '<tr>' );
+
+        var task_name = $( '<td>' ).text( event_needed_role['needed_role_name'] );
+        var actions   = create_action_set( row, event_needed_role_pk, event_needed_role['needed_role'] );
+
+        row.append(
+            task_name,
+            actions
+        );
+
+        task_list_tbody.append( row );
+    });
+
+    add_new_row();
+}
+
 function create_new_role()
 {
-    var new_name = $( '#new_name' ).val();
+    var new_name  = $( '#new_name' ).val();
+    var post_url  = 'http://planmything.tech/api/roles/';
+    var post_data = { 'name' : new_name };
+
+    var new_needed_role;
+    var new_event_needed_role;
+
+    $.post( post_url, post_data, function( response, textStatus, jqXHR ) {
+        new_needed_role = response['role'];
+        console.log( new_needed_role );
+    })
+    .done( function() {
+        post_url  = 'http://planmything.tech/api/event/' + TEST_EVENT + '/roles/';
+        post_data = { 'needed_role' : new_needed_role };
+
+        $.post( post_url, post_data, function( response, textStatus, jqXHR ) {
+            new_event_needed_role = response['event_needed_role'];
+            console.log( new_event_needed_role );
+        });
+    });
+
+    var new_row = $( '#new' );
+    new_row.empty();
+
+    var task_name = $( '<td>' ).text( new_name );
+    var actions   = create_action_set( new_row, new_event_needed_role, new_needed_role );
+
+    new_row.append(
+        task_name,
+        actions
+    );
+
+    new_row.removeAttr( 'id' );
+    add_new_row();
 }
 
 function perform_edit_task_update()
